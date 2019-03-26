ea749b10bbec2a3fb845d6ab907d32113ed5ea04
Russell Brown <russellpbrown@gmail.com>
2019-03-26 00:41:58 -0400

    Added game square divs

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOMContentLoaded')
+  let game = document.getElementById('game')
+
+  for (let i = 0; i < 9; i++) {
+    let gameSquare = document.createElement('div')
+    gameSquare.className = 'square' + i
+    game.appendChild(gameSquare)
+  }
+
 })
